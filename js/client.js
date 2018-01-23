9d1bbadb413ff434a1d91ad50046fe54bd68123d
matukai <matt.tanji@gmail.com>
2018-01-23 12:01:20 -1000

    clients talking to each other. work on client echo

--- js/client.js
+++ js/client.js
@@ -1,1 +1,30 @@
 const net = require('net');
+
+let client = net.createConnection({ port: 6969 , host: `0.0.0.0`}, () => {
+  //connect listener
+  console.log('client: connected to server!');
+
+
+//piping data to connection
+  process.stdin.pipe(client);
+});
+
+//coming into socket
+client.on('data', (data) => {
+  console.log(data.toString());
+});
+//harcoded writing to socket
+client.write('world\r\n');
+
+
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('readable', () => {
+//   const chunk = process.stdin.read();
+//   if (chunk !== null) {
+//     process.stdout.write(`data: ${chunk}`);
+//   }
+// });
+
+// process.stdin.on('end', () => {
+//   process.stdout.write('end');
+// });
--- js/server.js
+++ js/server.js
@@ -1,1 +1,34 @@
 const net = require('net');
+let requests = [];
+const server = net.createServer((req) => {
+  console.log('server: client connected');
+  requests.push(req);
+  //end when client disconnects
+  req.on('end', () => {
+    console.log('server: client disconnected');
+  });
+
+  //req is an instance of connection
+  req.on('data', function (chunk) {
+    req.setEncoding('utf8')
+    //console.log(chunk.toString());
+    //req.write(chunk)
+    requests.forEach( function (element) {
+      element.write(chunk);
+    })
+  });
+  req.write('server: hello\r\n', );
+});
+
+//throw error if server is not found
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(6969, function listen() {
+  console.log('server bound');
+});
+
+server.on('connection', (stream) => {
+  console.log('server: someone connected');
+});
